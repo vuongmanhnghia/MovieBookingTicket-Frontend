3ceaf1246fad32458e1939c59ff57c4bf9361c68
Vuong Manh Nghia <vuongmanhnghia@gmail.com>
2024-06-05 16:48:04 +0700

    Design banner

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -19,11 +19,44 @@ class HomeHeader extends Component {
 							<div className="nav-text">Phim chiếu</div>
 							<div className="nav-text">Review phim</div>
 							<div className="nav-text">Top phim</div>
-							<div className="nav-text">Block phim</div>
-							<div className="nav-text">
+							<div className="nav-text">Blog phim</div>
+							<div className="input-search">
+								<input type="text" placeholder="Nhập tên phim" />
 								<i class="fas fa-search"></i>
 							</div>
 						</div>
+					</div>
+				</div>
+				<div className="home-header-banner-container">
+					<div className="home-header-banner-content">
+						<div className="home-header-banner-booking">
+							<div className="banner-content-title">
+								Đặt mua vé xem phim
+							</div>
+							<div className="banner-content-check">
+								<ul className="list-booking-check">
+									<li>
+										<i class="fas fa-check-circle"></i>Mua vé online,
+										trải nghiệm phim hay
+									</li>
+									<li>
+										<i class="fas fa-check-circle"></i>Đặt vé an toàn
+									</li>
+									<li>
+										<i class="fas fa-check-circle"></i>Tha hồ chọn chỗ
+										ngồi, mua bắp nước tiện lợi
+									</li>
+									<li>
+										<i class="fas fa-check-circle"></i>Lịch sử đặt vé
+										được lưu lại ngay
+									</li>
+								</ul>
+							</div>
+							<div className="btn-booking-content">
+								<div className="btn-booking">Đặt vé ngay</div>
+							</div>
+						</div>
+						<div className="home-header-banner"></div>
 					</div>
 				</div>
 			</React.Fragment>
