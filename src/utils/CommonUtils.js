5f5c47073c9cd6bf3ba1d5092e6adbc0b67f12e0
Vuong Manh Nghia <vuongmanhnghia@gmail.com>
2024-06-12 18:55:41 +0700

    update file commonUtils.js

--- src/utils/CommonUtils.js
+++ src/utils/CommonUtils.js
@@ -1,8 +1,4 @@
 class CommonUtils {
-	static isNumber1(number) {
-		if (number === 1) return true;
-		return false;
-	}
 	static getBase64(file) {
 		return new Promise((resolve, reject) => {
 			const reader = new FileReader();
