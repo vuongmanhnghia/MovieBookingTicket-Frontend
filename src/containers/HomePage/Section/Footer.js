f96f8152a5f464430f01a77ba5255b3c6ac4dbcb
Vuong Manh Nghia <vuongmanhnghia@gmail.com>
2024-06-19 13:56:30 +0700

    css footer

--- src/containers/HomePage/Section/Footer.js
+++ src/containers/HomePage/Section/Footer.js
@@ -6,30 +6,23 @@ class Footer extends Component {
 	render() {
 		return (
 			<div className="section-footer-container">
-				<div className="row footer-container-content">
-					<div className="col footer-content">
-						<div className="title-footer">MUA VÉ XEM PHIM</div>
-						<div className="text-content">Lịch chiếu phim</div>
-						<div className="text-content">Rạp chiếu phim</div>
-						<div className="text-content">Phim chiếu rạp</div>
+				<div className="footer-container-content">
+					<div className="footer-content">
+						<div>MUA VÉ XEM PHIM</div>
+						<div>LỊCH CHIẾU PHIM</div>
+						<div>RẠP CHIẾU PHIM</div>
+						<div>PHIM CHIẾU RẠP</div>
 					</div>
-					<div className="col footer-content">
-						<div className="title-footer">DỊCH VỤ NỔI BẬT</div>
-						<div className="text-content"></div>
-						<div className="text-content"></div>
-						<div className="text-content"></div>
-						<div className="text-content"></div>
-						<div className="text-content"></div>
-					</div>
-					<div className="col footer-content">
-						<div className="title-footer">CHĂM SÓC KHÁCH HÀNG</div>
-						<div className="text-content"></div>
-						<div className="text-content"></div>
-						<div className="text-content"></div>
-						<div className="text-content"></div>
-					</div>
-					<div className="col footer-content">
-						<div className="title-footer">KẾT NỐI VỚI TÔI</div>
+					<div className="call-me">
+						<div className="call-me-text">KẾT NỐI VỚI TÔI</div>
+						<a
+							href="https://www.facebook.com/vuongmanhnghia365/"
+							target="blank">
+							<i class="fab fa-facebook"></i>
+						</a>
+						<a href="https://github.com/vuongmanhnghia" target="blank">
+							<i class="fab fa-github-square"></i>
+						</a>
 					</div>
 				</div>
 				<div className="more-info-container">
