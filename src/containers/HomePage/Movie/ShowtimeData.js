58f07ef5d8838b0870c576c637081e7b796e7c50
Vuong Manh Nghia <vuongmanhnghia@gmail.com>
2024-06-21 22:24:27 +0700

    update section showtime

--- src/containers/HomePage/Movie/ShowtimeData.js
+++ src/containers/HomePage/Movie/ShowtimeData.js
@@ -19,16 +19,20 @@ class ShowtimeData extends Component {
 
 			dataShowtime: {},
 			dataScreen: {},
+
+			imageTradeMark: "",
 		};
 	}
 
 	async componentDidMount() {
 		await this.props.fetchAllTradeMarks();
 		// lọc các phần tử giống nhau trong allTrademarks
-		let allTradeMarks = this.props.allTradeMarks.filter(
-			(item, index, self) =>
-				index === self.findIndex((t) => t.trademark === item.trademark)
-		);
+
+		let allTradeMarks = this.filterTradeMarks(this.props.allTradeMarks);
+		// let allTradeMarks = this.props.allTradeMarks.filter(
+		// 	(item, index, self) =>
+		// 		index === self.findIndex((t) => t.trademark === item.trademark)
+		// );
 		this.setState({
 			allTradeMarks: allTradeMarks,
 		});
@@ -61,26 +65,6 @@ class ShowtimeData extends Component {
 			});
 		});
 
-		// let activeLogo = document.querySelectorAll("div.logo-cinema");
-		// activeLogo.forEach((item) => {
-		// 	item.addEventListener("click", () => {
-		// 		document
-		// 			.querySelectorAll("div.logo-cinema")
-		// 			.forEach((item) => item.classList.remove("active"));
-		// 		item.classList.add("active");
-		// 	});
-		// });
-
-		// let activeTradeMark = document.querySelectorAll("div.tradeMark-cinema");
-		// activeTradeMark.forEach((item) => {
-		// 	item.addEventListener("click", () => {
-		// 		document
-		// 			.querySelectorAll("div.tradeMark-cinema")
-		// 			.forEach((item) => item.classList.remove("active-tradeMark"));
-		// 		item.classList.add("active-tradeMark");
-		// 	});
-		// });
-
 		let activeTime = document.querySelectorAll(".box-showtime");
 		activeTime.forEach((item) => {
 			item.addEventListener("click", () => {
@@ -89,6 +73,18 @@ class ShowtimeData extends Component {
 					.forEach((item) => item.classList.remove("active"));
 				item.classList.add("active");
 			});
+		});
+	}
+
+	filterTradeMarks(items) {
+		const seenNames = new Set();
+		return items.filter((item) => {
+			if (seenNames.has(item.tradeMark)) {
+				return false; // Bỏ qua các đối tượng có tên trùng
+			} else {
+				seenNames.add(item.tradeMark); // Lưu lại tên đã gặp
+				return true; // Giữ lại đối tượng đầu tiên có tên này
+			}
 		});
 	}
 
@@ -104,7 +100,13 @@ class ShowtimeData extends Component {
 	};
 
 	handleChangeCinema = async (tradeMark) => {
-		console.log(tradeMark);
+		await this.state.allTradeMarks.map((item, index) => {
+			if (item.tradeMark === tradeMark) {
+				this.setState({
+					imageTradeMark: item.image,
+				});
+			}
+		});
 		await this.setState({
 			showtimeCinema: await tradeMark,
 		});
@@ -117,33 +119,47 @@ class ShowtimeData extends Component {
 		let dataView = this.props.showtimeData;
 		let date = this.state.showtimeDate;
 		let tradeMark = this.state.showtimeCinema;
-		let result = [];
+		let arrResult = [];
 
 		await dataView.map((item) => {
 			if (
 				new Date(item.startDate).getDate() === date &&
 				item.tradeMarkId === tradeMark
 			) {
-				console.log(item);
-				result.push(item);
+				arrResult.push(item);
 			} else if (date === "" && item.tradeMarkId === tradeMark) {
-				result.push(item);
-			} else if (
-				new Date(item.startDate).getDate() === date &&
-				tradeMark === ""
-			) {
-				result.push(item);
+				arrResult.push(item);
 			}
+			// else if (
+			// 	new Date(item.startDate).getDate() === date &&
+			// 	tradeMark === ""
+			// ) {
+			// 	arrResult.push(item);
+			// }
 		});
+
+		let result = this.groupCinema(arrResult);
 		return result;
 	};
+
+	groupCinema(arrShowtime) {
+		const groups = arrShowtime.reduce((acc, item) => {
+			const group = acc[item.cinemaId] || [];
+			group.push(item);
+			acc[item.cinemaId] = group;
+			return acc;
+		}, {});
+
+		return Object.keys(groups).map((cinemaId) => ({
+			cinemaId,
+			arrShowtime: groups[cinemaId],
+		}));
+	}
 
 	handleViewDetailMovie = async (item) => {
 		await this.props.history.push(`/cinema/detail-movie/${item.id}`);
 		await this.props.history.push(`/detail-movie/${item.id}`);
 		await this.props.fetchDetailMovie(item.id);
-
-		console.log(this.props);
 	};
 
 	handleClickBooking = async (item) => {
@@ -163,7 +179,14 @@ class ShowtimeData extends Component {
 
 	render() {
 		let { allMovies, image, title } = this.props;
-		let { isOpenModal, dataShowtime, dataScreen, allTradeMarks } = this.state;
+		let {
+			isOpenModal,
+			dataShowtime,
+			dataScreen,
+			allTradeMarks,
+			showtimeData,
+			imageTradeMark,
+		} = this.state;
 		let maxDate = [1, 2, 3, 4, 5, 6, 7];
 		// let releaseTime = this.props.showtimeData.;
 		return (
@@ -219,19 +242,38 @@ class ShowtimeData extends Component {
 									})}
 							</div>
 							<div className="showtimeData-showtime">
-								{this.state.showtimeData &&
-									this.state.showtimeData.length > 0 &&
-									this.state.showtimeData.map((item) => {
-										return (
-											<div
-												className="box-showtime"
-												onClick={() =>
-													this.handleClickBooking(item)
-												}>
-												{item.startTime}
+								{this.state.showtimeData.map((item) => {
+									console.log("item:", item);
+									return (
+										<div className="box-showtime-cinema">
+											<div className="cinema-info">
+												<div
+													className="logo-cinema"
+													style={{
+														background: `url(${imageTradeMark})`,
+													}}></div>
+												<div className="name-cinema">
+													{item.cinemaId}
+												</div>
 											</div>
-										);
-									})}
+											<div className="list-box-showtime">
+												{item.arrShowtime &&
+													item.arrShowtime.length > 0 &&
+													item.arrShowtime.map((item, index) => {
+														return (
+															<div
+																className="box-showtime"
+																onClick={() =>
+																	this.handleClickBooking(item)
+																}>
+																{item.startTime}
+															</div>
+														);
+													})}
+											</div>
+										</div>
+									);
+								})}
 							</div>
 						</div>
 					</div>
